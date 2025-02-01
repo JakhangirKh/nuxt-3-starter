d088feccea94b9f96b7e077bd33bd82112695532
jaxangir <example@gmail.com>
2025-02-01 16:58:56 +0500

    exam

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    
-    <!-- <HomeRecommendedSection /> -->
-    
+    <div class="max-w-3xl mx-auto mt-10 p-6 ">
+      <h1 class="text-3xl font-bold text-center mb-6 text-gray-800">Users</h1>
+      <ul class="space-y-4">
+        <li v-for="user in users" :key="user.id" class="p-4 text-white bg-blue-600 rounded-lg hover:text-blue-600 hover:bg-white border hover:border-blue-600 flex justify-between items-center">
+          <NuxtLink :to="`/user/${user.id}`" class="text-black hover:text-blue-600 transition-colors duration-300">
+            {{ user.name }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
+<script setup>
+const { data: users } = await useAsyncData('users', () => $fetch('https://jsonplaceholder.typicode.com/users'));
+</script>
---
+++ pages/user/[id].vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="max-w-xl mx-auto mt-10 p-6 bg-white rounded-xl shadow-lg">
+    <h1 class="text-2xl font-bold mt-4 border-l-4 border-indigo-500 pl-3">
+      {{ user.name }}
+    </h1>
+
+    <div class="mt-4 space-y-2">
+      <p class="text-gray-600 gap-8">
+        <strong class="">Email:</strong> {{ user.email }}
+      </p>
+      <p class="text-gray-600 gap-8">
+        <strong class="">Phone:</strong> {{ user.phone }}
+      </p>
+      <p class="text-gray-600 gap-8">
+        <strong class="">Website:</strong>
+        <a
+          :href="`https://${user.website}`"
+          target="_blank"
+          class="text-indigo-600 hover:text-indigo-800 transition-colors duration-300"
+        >
+          {{ user.website }}
+        </a>
+      </p>
+    </div>
+    <NuxtLink
+      to="/"
+      class="text-white bg-blue-600 p-3 rounded-lg hover:text-blue-600 hover:bg-white border hover:border-blue-600 transition-colors duration-300 flex items-center justify-center mt-10"
+    >
+      &larr; Back
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const { data: user } = await useAsyncData(`user-${route.params.id}`, () =>
+  $fetch(`https://jsonplaceholder.typicode.com/users/${route.params.id}`)
+);
+</script>
